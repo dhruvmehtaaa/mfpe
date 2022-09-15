ced40ff6051ae5e49bd5e12fb24dea69cc38b456
dhruvmehtaaa <dhruvmehtaalt@gmail.com>
2022-09-15 19:48:28 +0530

    Adding ms

---
+++ audit-authorization/target/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (1, 'Dhruv Mehta', 'dm', 'dm000', 'Project-1');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (2, 'Nidhi R', 'nr', 'nr000', 'Project-2');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (3, 'Sheethal Madhu', 'sm', 'sm000', 'Project-3');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (4, 'Sumanth N', 'sn', 'sn000', 'Project-4');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (5, 'Mandeep Singh', 'ms', 'ms000', 'Project-5');
